e0eaf8e49fc3d47b5a4b35299c3b98616f637e7e
DevBhargav07 <bhargavdevarampati24@gmail.com>
2026-01-11 13:41:59 +0530

    remvoing time, merging and minimizing code for better use

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #still we didn't finished the count of times next time the user is trying to use the rockpapper sissors only 2 tries are coming.
-import random, pyfiglet, time
+import random, pyfiglet
 
 
 userCount = 0; programCount = 0; tied = 0
 rps = ["Rock", "Papper", "Sissors"]
-emojis_prg = {1: "\U0001f600", 2:"\U0001F606", 3: "\U0001F923"}
+emojis_prg = {1: "\U0001f600", 2:"\U0001F606", 3: "\U0001F923", "claps": "\U0001F44F"}
+computer_messages = {1: 'Let"s play again i"ll win this time.',2 : "Yeah... Yeah.....You Won.You know what time it is payback time.", 3: f"Suck...! Oops You lost..{emojis_prg[3]}"}
 
 def do_play():
 	random.shuffle(rps)
@@ -18,46 +19,43 @@ def take_input(userName):
 	try:
 		while True:
 			if not userName: userName="User"
-			userInput = input(f"Hi {userName}, Take One in Rock, Papper & Sissors: ") 
-			userInput = userInput.strip(' ')
-			if userInput not in rps: 
+			userInput = input(f"Hi {userName}, Take One in Rock, Papper & Sissors: ").strip()
+			formattedInput = userInput.capitalize()
+			if formattedInput not in rps: 
 				print(f'Please choose in {rps}')
-			elif userInput:
-				return userInput
-	
+			else:
+				return formattedInput
 	except KeyboardInterrupt:
 		print(f'Pleaseeee come back!!!')
+		return None
 
 def print_success(userName):
 	success_msg = pyfiglet.figlet_format("Congracts!! {}".format(userName), font="slant")
 	print(success_msg)
 
 userName = input("Hi! May i know your name: ")
-userInput = take_input(userName)
 
 try:
-	while userCount+programCount+tied <= 3:
-		ProgrammGuess = do_play()
+	while True:
+		userInput = take_input(userName)
 		if not userInput: break
-		if ProgrammGuess.lower() == userInput.lower():
+		ProgrammGuess = do_play()
+		if ProgrammGuess== userInput:
 			tied += 1
-			print(f'You both choose the same!')
-		elif ProgrammGuess == "Rock" and userInput == "Papper":
+			print(f"You both choose the {ProgrammGuess}, It's a tie")
+		elif (ProgrammGuess == "Rock" and userInput == "Papper") or \
+			(ProgrammGuess == "Papper" and userInput == "Sissors") or \
+			(ProgrammGuess == "Sissors" and userInput == "Rock") :
 			userCount += 1
-			print(f'Computer: Yeah i know its Best Of three right')
-		elif ProgrammGuess == "Papper" and userInput == "Sissors":
-			userCount += 1
-			print(f'Computer:Yes, You won. Let"s play again i"ll win this time.')
-		elif ProgrammGuess == "Sissors" and userInput == "Rock":
-			userCount += 1
-			print(f'Computer:Yes, You won. You know what time it is payback time.')
+			print(f'Computer choose: {ProgrammGuess}, You won {emojis_prg["claps"]}')
+			print()
+			print(f'Computer: {computer_messages[programCount]}')
 		else:
 			programCount += 1
 			print(f'Computer: {emojis_prg[programCount]} Sorry! You lost ')
 		if userCount + programCount+tied == 3:
 			print(f'Evaluating results')
 			print('.....................')
-			time.sleep(2)
 			if userCount > programCount and userCount > tied:
 				print(f'{userName} Won')
 				print_success(userName)
